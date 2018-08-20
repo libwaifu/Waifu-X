d91d31933b573593c0f8cfcbf136730a2610345a
GalAster <galaster@foxmail.com>
2018-08-20 20:30:30 +0800

    模块::Use template

--- Packages/WaifuDirectory.m
+++ Packages/WaifuDirectory.m
@@ -1,8 +1,42 @@
+(* ::Package:: *)
+(* ::Title:: *)
+(*Directories(Directories)*)
+(* ::Subchapter:: *)
+(*程序包介绍*)
+(* ::Text:: *)
+(*Mathematica Package*)
+(*Created by Mathematica Plugin for IntelliJ IDEA*)
+(*Establish from GalAster's template(v1.3)*)
+(**)
+(* ::Text:: *)
+(*Author:Aster*)
+(*Creation Date:2018-08-21*)
+(*Copyright: Mozilla Public License Version 2.0*)
+(* ::Program:: *)
+(*1.软件产品再发布时包含一份原始许可声明和版权声明。*)
+(*2.提供快速的专利授权。*)
+(*3.不得使用其原始商标。*)
+(*4.如果修改了源代码，包含一份代码修改说明。*)
+(**)
+(* ::Text:: *)
+(*这里应该填这个函数的介绍*)
+(* ::Section:: *)
+(*函数说明*)
 $WaifuDirectory::usage = "WaifuX 的安装路径.";
 $WaifuData::usage = "WaifuX 的数据存放位置.";
 $WaifuResourcesList::usage = "WaifuX 的远程资源映射表";
 $WaifuModelLoader::usage = "WaifuX 的资源加载器.";
+(* ::Section:: *)
+(*程序包正体*)
+(* ::Subsection::Closed:: *)
+(*主设置*)
 Begin["`Directories`"];
+(* ::Subsection::Closed:: *)
+(*主体代码*)
+Version$Directories = "V1.0";
+Updated$Directories = "2018-08-21";
+(* ::Subsubsection:: *)
+(*功能块 1*)
 $WaifuDirectory = DirectoryName[FindFile["Waifu`Kernel`"], 2];
 $WaifuData = FileNameJoin[{$UserBaseDirectory, "ApplicationData", "WaifuX"}];
 $WaifuResourcesList = <|
@@ -11,6 +45,11 @@ $WaifuResourcesList = <|
 		"Local" -> FileNameJoin[{$WaifuData, "Models", "Waifu-VDSR.WMLF"}]
 	|>
 |>;
+
+
+
+(* ::Subsubsection:: *)
+(*功能块 2*)
 downloadProgress[manifest_][event_] := manifest = <|manifest, event["Task"] -> event|>;
 downloadFinished[manifest_][event_] := NotebookDelete /@ NotebookFind[SelectedNotebook[], "Print", All, CellStyle];
 downloadStart[manifest_][src_, dest_] := URLDownloadSubmit[
@@ -38,21 +77,26 @@ downloadVisualize[manifest_] := If[
 	]&[First@Values[manifest]]
 ];
 $WaifuModelLoader::lost = "`1` 模型加载失败.";
-$WaifuModelLoader[name_] := If[
+$WaifuModelLoader[var_, name_] := If[
 	FileExistsQ@$WaifuResourcesList[name, "Local"],
-	Import[FileExistsQ@$WaifuResourcesList[name, "Local"]],
+	var = Import[$WaifuResourcesList[name, "Local"]],
 	manifest = <||>;
 	downloadStart[manifest][$WaifuResourcesList[name, "Remote"], $WaifuResourcesList[name, "Local"]];
 	Print@Dynamic@downloadVisualize[manifest];
-	Message[$WaifuModelLoader::lost, name];Return[$Failed]
+	Message[$WaifuModelLoader::lost, name];
+	Return[Clear[var]]
 ];
-Quiet[CreateDirectory/@{
+
+
+(* ::Subsection::Closed:: *)
+(*附加设置*)
+If[!FileExistsQ@#, CreateDirectory@#]& /@ {
 	$WaifuData,
 	FileNameJoin[{$WaifuData, "Models"}]
-}];
+};
 SetAttributes[{downloadProgress, downloadFinished, downloadStart, downloadVisualize}, HoldFirst];
 SetAttributes[
 	{$WaifuResourcesList, $WaifuModelLoader},
-	{Protected, ReadProtected}
+	{ReadProtected}
 ];
-End[];
+End[]
--- Packages/WaifuSR.m
+++ Packages/WaifuSR.m
@@ -1,11 +1,45 @@
+(* ::Package:: *)
+(* ::Title:: *)
+(*WaifuSR(WaifuSR)*)
+(* ::Subchapter:: *)
+(*程序包介绍*)
+(* ::Text:: *)
+(*Mathematica Package*)
+(*Created by Mathematica Plugin for IntelliJ IDEA*)
+(*Establish from GalAster's template(v1.3)*)
+(**)
+(* ::Text:: *)
+(*Author:Aster*)
+(*Creation Date:2018-08-21*)
+(*Copyright: Mozilla Public License Version 2.0*)
+(* ::Program:: *)
+(*1.软件产品再发布时包含一份原始许可声明和版权声明。*)
+(*2.提供快速的专利授权。*)
+(*3.不得使用其原始商标。*)
+(*4.如果修改了源代码，包含一份代码修改说明。*)
+(**)
+(* ::Text:: *)
+(*这里应该填这个函数的介绍*)
+(* ::Section:: *)
+(*函数说明*)
 WaifuSR::usage = "";
-
-Begin["`WaifuSR``"];
+(* ::Section:: *)
+(*程序包正体*)
+(* ::Subsection::Closed:: *)
+(*主设置*)
+Begin["`WaifuSR`"];
+(* ::Subsection::Closed:: *)
+(*主体代码*)
+Version$WaifuSR = "V1.0";
+Updated$WaifuSR = "2018-08-21";
+(* ::Subsubsection:: *)
+(*功能块 1*)
 WaifuSR = "";
 
 
-
-VDSR = Import@FileNameJoin[{NotebookDirectory[], "Waifu-VDSR.WMLF"}]
+(* ::Subsubsection:: *)
+(*功能块 2*)
+VDSR := $WaifuModelLoader[VDSR, "Waifu-VDSR.WMLF"];
 rgbMatrix = {{0.257, 0.504, 0.098}, {-0.148, -0.291, 0.439}, {0.439, -0.368, -0.071}};
 rgbMatrixT = {{1.164, 0., 1.596}, {1.164, -0.392, -0.813}, {1.164, 2.017, 0.}};
 WaifuVDSR[img_, zoom_ : 2, device_ : "GPU"] := Block[
@@ -19,4 +53,11 @@ WaifuVDSR[img_, zoom_ : 2, device_ : "GPU"] := Block[
 	ImageApply[rgbMatrixT.# + {-0.874, 0.532, -1.086}&, adjust @@ ColorSeparate[ycbcr]]
 ];
 
+
+(* ::Subsection::Closed:: *)
+(*附加设置*)
+SetAttributes[
+	{WaifuSR},
+	{Protected, ReadProtected}
+];
 End[]
